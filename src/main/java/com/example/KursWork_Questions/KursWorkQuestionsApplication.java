3adfffee31ed36c0b22f73733ae22990aa424c94
Elena <lenulka.90@mail.ru>
2023-10-13 11:42:28 +0300

    Initial commit.

---
+++ src/main/java/com/example/KursWork_Questions/KursWorkQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.KursWork_Questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursWorkQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursWorkQuestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KursWork_Questions/KursWorkQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KursWork_Questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursWorkQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
